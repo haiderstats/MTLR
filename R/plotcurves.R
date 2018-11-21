1adffa2be0948d93e0ce3f595b72ea521fb02939
Humza Haider <humza@haiderstats.com>
2018-11-21 13:17:12 -0700

    Add ability to plot survival curves.

---
+++ R/plotcurves.R
@@ -0,0 +1,58 @@
+
+#' Plot the survival curves returned from MTLR with a spline fit. If over 15 survival curves are plotted the legend is removed.
+#'
+#' @param curves survival curves formatted the same as those from predict.mtlr. The time points must be in the first column.
+#' @param index the index of the observation to plot. Here "1" will refer to the second column of curves
+#' @param color the color of the plotted survival curve. The length of color must match the length of index.
+#' @param xlim the limits of the x-axis (must be a 2 length vector).
+#' @export
+plotcurves <- function(curves, index = 1, color = c(), xlim = c()){
+  if (!requireNamespace(c("ggplot2","reshape2"), quietly = TRUE)) {
+    stop("Package \"ggplot2\" and \"reshape2\" are needed for this function to work. Please install them.",
+         call. = FALSE)
+  }
+  colorOK <- T
+  if(!length(color))
+    colorOK <- F
+  else if(length(color) != length(index)){
+    warning("If you would like to select custom colors please make sure the number of colors
+            matches the number of curves.")
+    colorOK <- F
+  }
+  time <- curves[,1]
+  curves <- curves[,index +1,drop=F]
+  plot_times <- seq(min(time),max(time), length.out = length(time)*100)
+  plot_probs <- as.data.frame(sapply(curves,
+                                   function(curve){
+                                     curve = ifelse(curve < 1e-20,0,curve)
+                                     survivialSpline = stats::splinefun(time, curve, method = "hyman")
+                                     return(pmax(survivialSpline(plot_times),0))
+                                   }
+  ))
+  plot_data = cbind.data.frame(plot_times,plot_probs)
+  plot_data = reshape2::melt(plot_data,measure.vars = names(plot_data)[-1], variable.name = "Index")
+  pl = ggplot2::ggplot(data = plot_data, ggplot2::aes(x = plot_times,y = plot_data$value, colour = plot_data$Index))+
+    ggplot2::geom_line(size = 1.5)
+  if(colorOK)
+    pl = pl + ggplot2::scale_color_manual(values = color)
+  if(length(xlim)==2){
+    pl = pl+ ggplot2::xlim(c(xlim[1],xlim[2]))
+  }
+  pl = pl +
+    ggplot2::scale_y_continuous( limits= c(0,1),breaks = c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1))+
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+          text = ggplot2::element_text(size=18, face=  "bold"),
+          axis.title = ggplot2::element_text(size = 20),
+          axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 15)),
+          axis.title.y = ggplot2::element_text(margin = ggplot2::margin(r = 15)))+
+    ggplot2::labs(y = "Survival Probability",x = "Time", color = "Index" )
+  #If we have too many survival curves then the legend takes up the whole plot. We have an if to catch this and remove the legend.
+  if(length(index) > 15){
+    pl = pl + ggplot2::theme(legend.position = "None")
+  }
+  return(pl)
+}
+
+
+
