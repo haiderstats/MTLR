799bf9e2998f9d34872ff03a98f699ab1107c070
Humza Haider <humza@haiderstats.com>
2018-11-27 13:12:32 -0700

    updating text to be consistent with mtlr_cv.

--- tests/testthat/test-mtlr_cv.R
+++ tests/testthat/test-mtlr_cv.R
@@ -9,27 +9,27 @@ testthat::test_that("mtlr_cv function is consistent for basic survival dataset",
   expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_leuk.rds")
 })
 
-testthat::test_that("mtlr function is consistent for more complex survival dataset",{
+testthat::test_that("mtlr_cv function is consistent for more complex survival dataset",{
   formula = survival::Surv(time,status)~.
   data = survival::lung
   expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_lung.rds")
 })
 
-testthat::test_that("mtlr function is consistent for all censored survival dataset",{
+testthat::test_that("mtlr_cv function is consistent for all censored survival dataset",{
   formula = survival::Surv(time,status)~.
   data = survival::lung
   data = data[data$status == 1,]
   expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_censored.rds")
 })
 
-testthat::test_that("mtlr function is consistent for all uncensored survival dataset",{
+testthat::test_that("mtlr_cv function is consistent for all uncensored survival dataset",{
   formula = survival::Surv(time,status)~.
   data = survival::leukemia
   data = data[data$status == 1,]
   expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_uncensored.rds")
 })
 
-testthat::test_that("mtlr function is consistent for all uncensored survival dataset",{
+testthat::test_that("mtlr_cv function is consistent for all uncensored survival dataset",{
   formula = survival::Surv(time,status)~.
   data = survival::leukemia
   data = data[data$status == 1,]
