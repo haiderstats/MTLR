5d172022ce21bca70063748f384fa6b288c74e69
Humza Haider <humza@haiderstats.com>
2018-11-30 09:09:04 -0700

    Adjust temporarily back for only testing and Fix error with selecting time points.

--- R/mtlr.R
+++ R/mtlr.R
@@ -93,26 +93,21 @@ mtlr <- function(formula,
 
   if (!survival::is.Surv(y))
     stop("The response must be a Surv object.")
-  type = attr(y,"type")
-  if(!type %in% c("right", "left","interval2"))
-    stop("Currently only right, left, and interval2 censored data is supported. See the Details section of ?mtlr.")
+  if(attr(y, "type") != "right")
+    stop("Currently only right censored data is supported.")
 
   time <- y[,1]
+  delta <- y[,2] #Death indicator (censored = 0, death = 1)
+
   if(any(time < 0))
-    stop("All event times must be non-negative.")
-  if(type == "interval2"){
-    time2 = y[,2]
-    delta <- y[,3] #Death indicator (right censored = 0, death = 1, left censored = 2, interval censored = 3)
-  }else{
-    delta <- y[,2] #Death indicator (right censored = 0, death = 1, left censored = 2, interval censored = 3)
-  }
+    stop("All event times must be positive.")
   if(C1 < 0)
     stop("C1 must be non-negative.")
   if(threshold <= 0)
     stop("The threshold must be positive.")
 
   if(is.null(nintervals))
-    nintervals <- sqrt(ceiling(nrow(y))) #Default number of intervals is the sq. root of the number of observations.
+    nintervals <- ceiling(sqrt(nrow(y))) #Default number of intervals is the sq. root of the number of observations.
   if(normalize){
     x <- scale(x)
     scale_centers <- attr(x, "scaled:center")
@@ -126,9 +121,7 @@ mtlr <- function(formula,
   #Prepare data for MTLR Rcpp functions.
   #The Rcpp functions are built to work on data that comes in with observations ordered by their event status
   #(censored observations first).
-  #Here we order our data by the censor status. However, since left and interval censored are coded as 2,3
-  #we make these negative first.
-  delta = ifelse(delta == 1,1,-delta)
+  #Here we order our data by the censor status.
   ord <- order(delta)
 
   if(ncol(x)){
@@ -144,23 +137,7 @@ mtlr <- function(formula,
 
   #We make a matrix where each column is a vector of indicators if an observation is dead at each time point, e.g. (0,0,,...,0,1,1,...1).
   #(See 'Learning Patient-Specific Cancer Survival Distributions as a Sequence of Dependent Regressors' Page 3.)
-  if(type =="right"){
-    y_matrix <- matrix(1 - Reduce(c,Map(function(ind) time[ord] > time_points[ind], seq_along(time_points))), ncol = length(time), byrow = T)
-  } else if(type == "left"){
-    y_cen <- matrix(1 - Reduce(c,Map(function(ind) time[ord] <= c(0,time_points)[ind], seq_along(time_points))), ncol = length(time_points), byrow = T)
-    y_uncen <- matrix(1 - Reduce(c,Map(function(ind) time[ord] > time_points[ind], seq_along(time_points))), nrow = length(time_points), byrow = T)
-    y_matrix <- cbind(y_cen,y_uncen)
-
-  } else{
-    y_int <- matrix(as.numeric(Reduce(c,Map(function(ind) time[delta == -3] <= c(time_points,Inf)[ind] & time2[delta == -3] > c(0,time_points)[ind],
-                                            seq_along(c(time_points,Inf))))), nrow = length(time_points)+1, byrow = T)
-    y_left <- matrix(as.numeric(Reduce(c,Map(function(ind) time[delta == -2] >= c(0,time_points)[ind],
-                                             seq_along(c(time_points,Inf))))), nrow = length(time_points) +1, byrow = T)
-    y_right <- matrix(as.numeric(Reduce(c,Map(function(ind) time[delta == 0] < c(time_points,Inf)[ind], seq_along(c(time_points,Inf))))), nrow = length(time_points)+1, byrow = T)
-    y_uncen <- matrix(as.numeric(Reduce(c,Map(function(ind) time[delta == 1] < c(time_points,Inf)[ind], seq_along(c(time_points,Inf))))), nrow = length(time_points)+1, byrow = T)
-    y_matrix <- cbind(y_int, y_left, y_right, y_uncen)
-
-  }
+  y_matrix <- matrix(1 - Reduce(c,Map(function(ind) time[ord] >= time_points[ind], seq_along(time_points))), ncol = length(time), byrow = T)
 
   #We first train the biases (by setting feature parameters to zero (dAsZero)). Then we train the feature values as if all patients
   #were uncensored (this creates "good" starting values since with censored patients the objective is non-convex). Then we finally
@@ -170,22 +147,22 @@ mtlr <- function(formula,
   if(train_biases){
     zero_matrix <- matrix(0,ncol = ncol(x), nrow = nrow(x))   #We create a zero_matrix to train the biases
 
-    bias_par <- stats::optim(par = rep(0,length(time_points)*(ncol(x) +1)),fn = mtlr_objVal2,gr = mtlr_grad2, yval = rbind(y_matrix,1),
-                     featureVal = zero_matrix, C1=C1, delta = sort(delta),
-                     method = "L-BFGS-B", lower = lower, upper = upper, control = c(maxit = maxit, factr = threshold_factor))
+    bias_par <- stats::optim(par = rep(0,length(time_points)*(ncol(x) +1)),fn = mtlr_objVal,gr = mtlr_grad, yval = y_matrix,
+                             featureVal = zero_matrix, C1=C1, delta = sort(delta),
+                             method = "L-BFGS-B", lower = lower, upper = upper, control = c(maxit = maxit, factr = threshold_factor))
     if(bias_par$convergence == 52)
       stop(paste("Error occured while training MTLR. Optim Error: ", bias_par$message))
   }else{
     bias_par <- list(par = rep(0,length(time_points)*(ncol(x) +1)))
   }
 
-  params_uncensored <- stats::optim(par = bias_par$par,fn = mtlr_objVal2, gr = mtlr_grad2, yval = rbind(y_matrix,1), featureVal = x, C1 = C1, delta = rep(1,nrow(x)),
-                       method = "L-BFGS-B", lower = lower, upper = upper, control = c(maxit = maxit, factr = threshold_factor))
+  params_uncensored <- stats::optim(par = bias_par$par,fn = mtlr_objVal, gr = mtlr_grad, yval = y_matrix, featureVal = x, C1 = C1, delta = rep(1,nrow(x)),
+                                    method = "L-BFGS-B", lower = lower, upper = upper, control = c(maxit = maxit, factr = threshold_factor))
   if(params_uncensored$convergence == 52)
     stop(paste("Error occured while training MTLR. Optim Error: ", params_uncensored$message))
 
-  final_params <- stats::optim(par = params_uncensored$par,fn = mtlr_objVal2,gr = mtlr_grad2, yval = rbind(y_matrix,1), featureVal = x, C1 = C1,delta = sort(delta),
-                    method = "L-BFGS-B", lower = lower, upper = upper, control = c(maxit = maxit, factr = threshold_factor))
+  final_params <- stats::optim(par = params_uncensored$par,fn = mtlr_objVal,gr = mtlr_grad, yval = y_matrix, featureVal = x, C1 = C1,delta = sort(delta),
+                               method = "L-BFGS-B", lower = lower, upper = upper, control = c(maxit = maxit, factr = threshold_factor))
   if(final_params$convergence == 52)
     stop(paste("Error occured while training MTLR. Optim Error: ", final_params$message))
 
