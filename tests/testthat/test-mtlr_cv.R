8baaff4fe406bf2103fc89b55e2ad5c290d57aaa
Humza Haider <humza@haiderstats.com>
2018-12-07 13:24:25 -0700

    Lower tolerance so other compiliers don't fail the tests.

--- tests/testthat/test-mtlr_cv.R
+++ tests/testthat/test-mtlr_cv.R
@@ -6,27 +6,27 @@ testthat::context("Testing the mtlr_cv function. Most tests are just checking ag
 testthat::test_that("mtlr_cv function is consistent for basic survival dataset",{
   formula = survival::Surv(time,status)~.
   data = survival::leukemia
-  expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_leuk.rds")
+  expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_leuk.rds", tolerance = 1e-2)
 })
 
 testthat::test_that("mtlr_cv function is consistent for more complex survival dataset",{
   formula = survival::Surv(time,status)~.
   data = survival::lung
-  expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_lung.rds", tolerance = 1e-3)
+  expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_lung.rds", tolerance = 1e-2)
 })
 
 testthat::test_that("mtlr_cv function is consistent for all censored survival dataset",{
   formula = survival::Surv(time,status)~.
   data = survival::lung
   data = data[data$status == 1,]
-  expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_censored.rds", tolerance = 1e-3)
+  expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_censored.rds", tolerance = 1e-2)
 })
 
 testthat::test_that("mtlr_cv function is consistent for all uncensored survival dataset",{
   formula = survival::Surv(time,status)~.
   data = survival::leukemia
   data = data[data$status == 1,]
-  expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_uncensored.rds")
+  expect_equal_to_reference(mtlr_cv(formula,data),"mtlrcv_uncensored.rds", tolerance = 1e-2)
 })
 
 testthat::test_that("mtlr_cv function catches negative C1 survival dataset",{
@@ -46,7 +46,7 @@ testthat::test_that("mtlr_cv function works with multiple types of censoring",{
                          importantfeature3 = rnorm(20),importantfeature4 = rnorm(20),importantfeature5 = rnorm(20),
                          importantfeature6 = rbinom(20,1,.3),importantfeature7 = rbinom(20,1,.3))
   formula = survival::Surv(time1,time2,type = "interval2")~.
-  expect_equal_to_reference(mtlr_cv(formula, dat, C1_vec = c(.001,.01,.1,0)),"mtlrcv_mixed_censoring.rds",tolerance = 1e-3)
+  expect_equal_to_reference(mtlr_cv(formula, dat, C1_vec = c(.001,.01,.1,0)),"mtlrcv_mixed_censoring.rds",tolerance = 1e-2)
 })
 
 
