401d00e8cb2ff8197f3c6be8f26381b0a219e4c5
Humza Haider <humza@haiderstats.com>
2019-01-17 15:02:31 -0700

    paste all variables at once.

--- R/mtlr.R
+++ R/mtlr.R
@@ -160,7 +160,7 @@ mtlr <- function(formula,
     scales <- list(center= scale_centers, sd = scale_scale)
     if(any(scale_scale ==0)){
       zeroVar = names(scale_scale)[which(scale_scale ==0)]
-      warning(paste("The feature(s): ", zeroVar, " have zero variance. These features have been set to zero to ensure a feature weight of zero."))
+      warning(paste("The feature(s): ", paste(zeroVar, collapse = " "), " have zero variance. These features have been set to zero to ensure a feature weight of zero."))
       x[,which(scale_scale ==0)] = 0
     }
   }else{
